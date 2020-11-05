252ac14596c2dbbbe30de800c50be7834ae842b8
Lowari <66225181+Lowari@users.noreply.github.com>
2020-11-05 17:22:25 +0100

    Correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,48 @@
-var firstName = document.querySelector("#firstName");
-var msgErrorName = document.querySelector('#errorName');
+const firstName = document.querySelector("#firstName");
+const msgErrorName = document.querySelector('#errorName');
+const regexFirstName = /[a-z]/i;
 
-var mail = document.querySelector('#email');
-var msgErrorMail = document.querySelector("#errorMail");
+const mail = document.querySelector('#email');
+const msgErrorMail = document.querySelector("#errorMail");
+const regexMail = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
-var age = document.querySelector("#age");
-var msgErrorAge = document.querySelector("#errorAge");
+const age = document.querySelector("#age");
+const msgErrorAge = document.querySelector("#errorAge");
+const regexAge = /[0-9]/i;
 
-var button = document.querySelector("#buttonPass");
+const button = document.querySelector("#buttonPass");
+
+const test = document.querySelector("#errorNamee")
 
 // Fonction pour l'input : Nom
 
 button.addEventListener("click", function () {
-    if (firstName.value.match(/[a-z]/i)) {
-        msgErrorName.setAttribute("hidden", "true");
+    if (regexFirstName.test(firstName.value) == true) {
+        msgErrorName.textContent = "";
     } else {
-        msgErrorName.setAttribute("class", "error");
-        msgErrorName.removeAttribute("hidden");
-    }
-})
-
-// Fonction pour l'input : Mail
-
-button.addEventListener("click", function () {
-    if (age.value.match(/[0-9]/i)) {
-        msgErrorAge.setAttribute("hidden", "true");
-    } else {
-        msgErrorAge.setAttribute("class", "error");
-        msgErrorAge.removeAttribute("hidden");
+         msgErrorName.setAttribute("class", "error");
+         msgErrorName.textContent = "Erreur saisie ! Ex : Dupont";
     }
 })
 
 // Fonction pour l'input : Age
 
 button.addEventListener("click", function () {
-    if (mail.value.match(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i)) {
-        msgErrorMail.setAttribute("hidden", "true");
+    if (regexAge.test(age.value) == true) {
+        msgErrorAge.textContent = "";
+    } else {
+        msgErrorAge.setAttribute("class", "error");
+        msgErrorAge.textContent = "Erreur saisie, veuillez saisir des chiffres";
+    }
+})
+
+// Fonction pour l'input : Mail
+
+button.addEventListener("click", function () {
+    if (regexFirstName.test(mail.value) == true) {
+        msgErrorMail.textContent = "";
     } else {
         msgErrorMail.setAttribute("class", "error");
-        msgErrorMail.removeAttribute("hidden");
+        msgErrorMail.textContent = "Veuillez Respecter le bon format ! Ex : Exemple@mail.fr";
     }
 })
